e3dec05c25c939e412fd680712cf495740cf790b
Armianto Sumitro <armiantos@gmail.com>
2020-04-29 16:27:34 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# armiantos.github.io
-Personal portfolio
---
+++ precache-manifest.e15bb23747c6a1bba63726509a749d1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1753beb7e7c31deef7abdbf74e269c11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a514db2e4325bb5638c8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d57cdf1fea01c7714b12",
+    "url": "/static/js/2.ba90f5cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba90f5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a514db2e4325bb5638c8",
+    "url": "/static/js/main.c6720d2d.chunk.js"
+  },
+  {
+    "revision": "7a3d5585f798b286bef0",
+    "url": "/static/js/runtime-main.8f03611f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
