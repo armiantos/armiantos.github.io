8fb8115a8a56f9a117e85a33f741b1e2729610e2
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-20 15:25:22 -0400

    Customized header
    hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import anime from 'animejs';
 
 function App() {
+  useEffect(() => { 
+    anime({
+      targets: ".App-logo",
+      duration: 1000,
+      easing: "easeOutQuart",
+      rotate: 360,
+      scale: [0, 1],
+    });
+
+    anime({
+      targets: ".App-catchphrase",
+      duration: 2000,
+      opacity: [0, 1],
+      translateY: ['5vmin', 0],
+      easing: "easeOutQuart",
+      delay: 300
+    });
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p className="App-catchphrase">
+          I am <em>the</em> software engineer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
