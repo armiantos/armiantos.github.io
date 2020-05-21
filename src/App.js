e69ed76a9c987808b96ca09f46eabfadc6ded717
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-21 11:50:42 -0400

    Line draw logo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import anime from 'animejs';
+import React, { useEffect } from "react";
+import "./App.css";
+import anime from "animejs";
+
+import Logo from './Logo';
 
 function App() {
-  useEffect(() => { 
-    anime({
-      targets: ".App-logo",
-      duration: 1000,
-      easing: "easeOutQuart",
-      rotate: 360,
-      scale: [0, 1],
+  function animate() {
+    var tl = anime.timeline({
     });
 
-    anime({
+    tl
+    .add({
+      targets: ".App-logo path",
+      easing: 'easeInOutSine',
+      duration: 5000,
+      strokeDashoffset: [anime.setDashoffset, 0],
+    })
+    .add({
       targets: ".App-catchphrase",
       duration: 2000,
       opacity: [0, 1],
-      translateY: ['5vmin', 0],
-      easing: "easeOutQuart",
-      delay: 300
-    });
+      easing: 'easeOutQuart',
+      translateY: ["5vmin", 0],
+    }, '-=500')
+  }
+
+  useEffect(() => {
+    animate();
   });
-  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <Logo className="App-logo"/>
         <p className="App-catchphrase">
           I am <em>the</em> software engineer.
         </p>
