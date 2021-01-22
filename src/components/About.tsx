6bd1b4a6151ffb71f5ff61837bd8678ad1396cab
Armianto Sumitro <sumitro@ualberta.ca>
2021-01-22 02:25:37 -0700

    Remove animate and use material-ui container to center

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,35 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import anime from 'animejs';
 import './About.css';
-
-function animate() {
-    const tl = anime.timeline({});
-
-    tl.add({
-        targets: '.logo path',
-        easing: 'easeInOutSine',
-        duration: 5000,
-        strokeDashoffset: [anime.setDashoffset, 0],
-    }).add(
-        {
-            targets: '.catchphrase',
-            duration: 2000,
-            opacity: [0, 1],
-            easing: 'easeOutQuart',
-            translateY: ['5vmin', 0],
-        },
-        '-=500'
-    );
-}
 
 const useStyles = makeStyles((theme) => ({
     large: {
         width: theme.spacing(32),
         height: theme.spacing(32),
-        margin: `${theme.spacing(2)}px 0 ${theme.spacing(2)}px 0`,
+        margin: `${theme.spacing(2)}px auto`,
     },
     subtitle: {
         margin: `${theme.spacing(4)}px 0`,
@@ -39,26 +19,22 @@ const useStyles = makeStyles((theme) => ({
 function About() {
     const classes = useStyles();
 
-    useEffect(() => {
-        animate();
-    });
-
     return (
-        <div>
-            <div className="container about short">
+        <div className="container">
+            <Container maxWidth="md">
                 <Typography variant="h3" gutterBottom>
                     Hi!
                 </Typography>
-                
+
                 <Avatar alt="Me" src="/profile.JPG" className={classes.large} />
 
                 <Typography variant="body1">
                     My name is Armi. I am a 4<sup>th</sup> year Software Engineering Student at the University of Alberta.
                 </Typography>
                 <Typography variant="body1">
-                    My passion is to empower others by developing programs that will greatly enhance their productivity.
+                    My passion is to empower others by developing programs that will enhance their productivity and pleasing to look at.
                 </Typography>
-            </div>
+            </Container>
         </div>
     );
 }
