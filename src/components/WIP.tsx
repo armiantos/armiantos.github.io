08abf5ceaae3e4a4b1852512e79b4cac9dfc83f6
Armianto Sumitro <sumitro@ualberta.ca>
2021-01-22 01:56:53 -0700

    Use typography for 'Under construction'

--- src/components/WIP.tsx
+++ src/components/WIP.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         height: '100%',
+        padding: '50px 0'
     },
 }));
 
 function WIP() {
     const classes = useStyles();
-    
+
     return (
-        <Container maxWidth="sm" className={classes.root}>
-            Under construction
+        <Container maxWidth="md" className={classes.root}>
+            <Typography variant="h3">Under construction</Typography>
         </Container>
     )
 }
