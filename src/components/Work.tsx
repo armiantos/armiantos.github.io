6e0c51f895eaf8dd5acb0d07b555817d2b8e1f82
Armianto Sumitro <sumitro@ualberta.ca>
2021-02-17 00:31:37 -0700

    Add appear animations

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Experience from './Experience';
+import anime from 'animejs';
 import './Work.css';
 
 import work from '../data/work';
@@ -18,6 +19,17 @@ const useStyles = makeStyles((theme) => ({
 function Work() {
     const classes = useStyles();
 
+    useEffect(() => {
+        anime({
+            targets: '.experience',
+            translateY: ['-50px', 0],
+            opacity: [0, 1],
+            easing: 'easeOutCubic',
+            duration: 1000,
+            delay: anime.stagger(200),
+        });
+    }, []);
+
     return (
         <div className="Work">
             <Typography variant="h3" className={classes.title} gutterBottom>
@@ -27,7 +39,7 @@ function Work() {
                 <Grid container direction="row" justify="center" alignItems="center">
                     {work.map((experience) => (
                         <Grid item xs={12} key={experience.employer.name}>
-                            <Paper elevation={0}>
+                            <Paper elevation={0} className="experience">
                                 <Experience
                                     employer={experience.employer}
                                     location={experience.location}
