f488b7609ab6e1d0e2551f4f2dec5eb9d9170040
Armianto Sumitro <sumitro@ualberta.ca>
2021-02-18 00:49:06 -0700

    Merge Experience to work.tsx

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,14 +1,24 @@
 import React, { useEffect } from 'react';
+import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-import Experience from '../components/Experience';
+import Box from '@material-ui/core/Box';
 import anime from 'animejs';
-// import './Work.css'; TODO: Fix css imports
 
 import work from '../data/work';
-import Container from '@material-ui/core/Container';
+
+type ExperienceProps = {
+    employer: {
+        name: string;
+        logo?: string;
+    };
+    duration: Date[];
+    location: string;
+    title: string;
+    summary: string[];
+};
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,7 +28,78 @@ const useStyles = makeStyles((theme) => ({
     title: {
         marginTop: '50px',
     },
+    large: {
+        objectFit: 'contain',
+        boxSizing: 'border-box',
+        width: '100%',
+        height: '100%',
+        padding: '5%',
+        margin: 'auto',
+    },
+    h5: {
+        marginRight: '.5em',
+    },
 }));
+const classes = useStyles();
+
+function toShortString(date: Date): String {
+    const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+
+    return `${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+}
+
+const Work = ({ employer, location, title, summary, duration }: ExperienceProps) => {
+    const classes = useStyles();
+
+    return (
+        <Grid container justify="center" alignItems="center">
+            <Grid item sm={4}>
+                <img className={classes.large} alt={`${employer.name}-logo`} src={employer.logo} />
+            </Grid>
+            <Grid item sm={7}>
+                <Box display="flex" alignItems="baseline">
+                    <Typography variant="h5" align="left" className={classes.h5}>
+                        {employer.name}
+                    </Typography>
+                    <Typography variant="h6" align="left" color="textSecondary">
+                        {location}
+                    </Typography>
+                </Box>
+
+                <Typography variant="subtitle1" align="left">
+                    {title}
+                </Typography>
+
+                <Typography variant="subtitle1" color="textSecondary" align="left">
+                    {`${toShortString(duration[0])} - ${toShortString(duration[1])}`}
+                </Typography>
+
+                <ul>
+                    {summary.map((item, i) => (
+                        <li key={i}>
+                            <Typography variant="body1" align="left">
+                                {item}
+                            </Typography>
+                        </li>
+                    ))}
+                </ul>
+            </Grid>
+        </Grid>
+    );
+};
 
 const WorkPage = () => {
     const classes = useStyles();
@@ -44,7 +125,7 @@ const WorkPage = () => {
                     {work.map((experience) => (
                         <Grid item xs={12} key={experience.employer.name}>
                             <Paper elevation={0} className={classes.root + ' experience'}>
-                                <Experience
+                                <Work
                                     employer={experience.employer}
                                     location={experience.location}
                                     title={experience.title}
