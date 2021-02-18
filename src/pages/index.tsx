8ae3ddcd930e17a2678f37bd5677e23862999625
Armianto Sumitro <sumitro@ualberta.ca>
2021-02-17 21:11:21 -0700

    Add github and linkedin links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,14 @@ import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Spirals from '../components/Spirals';
 import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import IconButton from '@material-ui/core/IconButton';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import anime from 'animejs';
 
 import '../styles/index.css';
@@ -44,10 +49,19 @@ const IndexPage = () => {
                     <Typography variant="body1" align="left">
                         My name is Armi. I am a 4<sup>th</sup> year Software Engineering Student at the University of Alberta.
                     </Typography>
-                    <Typography variant="body1" align="left">
+                    <Typography variant="body1" align="left" gutterBottom>
                         My passion is to empower others by developing programs that will enhance their productivity and is visually
                         appealing.
                     </Typography>
+
+                    <Box display="flex" justifyContent="flex-start">
+                        <IconButton aria-label="source" component={Link} href={'https://github.com/armiantos'}>
+                            <GitHubIcon />
+                        </IconButton>
+                        <IconButton aria-label="source" component={Link} href={'https://www.linkedin.com/in/armiantos/'}>
+                            <LinkedInIcon />
+                        </IconButton>
+                    </Box>
                 </Paper>
             </Container>
         </div>
